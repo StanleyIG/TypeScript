a0cb7cfbfe47000ac92893db39987b22a17e0234
stanleyig <oootehts@gmail.com>
2026-03-06 19:46:00 +0300

    getter setter ready

--- test.ts
+++ test.ts
@@ -1,2 +1,27 @@
-// TypeScript
+class User {
+	_login: string;
+	password: string;
+	createdAt: Date;
 
+	set login(l: string | number) {
+		this._login = 'user-' + l;
+		this.createdAt = new Date();
+	}
+
+	get login() {
+		return this._login;
+	}
+
+	async getPassword(p: string) {
+
+	}
+
+	// set password(p: string) {
+	// 	// sync
+	// }
+}
+
+const user = new User();
+user.login = 'myLogin';
+console.log(user);
+console.log(user.login);
