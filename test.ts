8eb42bb074eab0942b2539be07c4deb6175c4572
stanleyig <oootehts@gmail.com>
2026-03-08 10:40:41 +0300

    fix code

--- test.ts
+++ test.ts
@@ -1,2 +1,32 @@
 // TypeScript
 
+class Vehicle {
+	public make: string;
+	private damages: string[];
+	private _model: string;
+	protected run: number;
+	#price: number;
+
+	set model(m: string) {
+		this._model = m;
+		this.#price = 100;
+	}
+
+	get model() {
+		return this._model;
+	}
+
+	addDamage(damage: string) {
+		this.damages.push(damage);
+	}
+}
+
+class EuroTruck extends Vehicle {
+	setRun(km: number) {
+		this.run = km / 0.62;
+		// this.damages - error
+	}
+}
+
+new Vehicle()
+new EuroTruck()
