e6c14f8b98ef57d1810574c2f3daf4e23652d0c2
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2026-04-06 17:51:11 +0300

    decorator accessor ready (#6)

--- src/test.ts
+++ src/test.ts
@@ -1,75 +1,38 @@
 interface IUserService {
-	users: number;
 	getUsersInDatabase(): number;
 }
 
 class UserService implements IUserService {
-	@Max(100)
-	users: number;
+	private _users: number;
+
+	@Log()
+	set users(num: number) {
+		this._users = num;
+	}
+
+	get users() {
+		return this._users;
+	}
 
 	getUsersInDatabase(): number {
 		throw new Error('Ошибка');
 	}
 }
 
-// function Max(max: number) {
-// 	return (
-// 		target: Object,
-// 		propertyKey: string | symbol
-// 	) => {
-// 		let value: number | undefined;
-//         console.log(`value: ${value}`)
-// 		const setter = function (newValue: number) {
-// 			if (newValue > max) {
-// 				console.log(`Нельзя установить значение больше ${max}`);
-// 			} else {
-// 				value = newValue;
-// 			}
-// 		}
-
-// 		const getter = function () {
-// 			return value;
-// 		}
-
-// 		Object.defineProperty(target, propertyKey, {
-// 			set: setter,
-// 			get: getter
-// 		});
-// 	}
-// }
-
-// Это можно сказать баг в коде. Для всех инстансов свойство users теперь будет 1 по дефолту.
-
-// Исправление:
-
-function Max(max: number) {
-    return (target: Object, propertyKey: string | symbol) => {
-        // Использовать WeakMap для хранения значений каждого экземпляра
-        const values = new WeakMap<any, number>();
-        
-        Object.defineProperty(target, propertyKey, {
-            set: function(newValue: number) {
-                if (newValue > max) {
-                    console.log(`Нельзя установить значение больше ${max}`);
-                } else {
-                    values.set(this, newValue);  // храним для конкретного экземпляра
-                }
-            },
-            get: function() {
-                return values.get(this);
-            }
-        });
-    }
+function Log() {
+	return (
+		target: Object,
+		_: string | symbol,
+		descriptor: PropertyDescriptor
+	) => {
+		const set = descriptor.set;
+		descriptor.set = (...args: any) => {
+			console.log(args);
+			set?.apply(target, args);
+		}
+	}
 }
-
-// new WeakMap<any, number>() Хранит слабые ссылки, так что запись автоматически исчезнет из WeakMap.
-// Код проверила языковая модель DeepSeek v3. Я просто спросил пару моментов, но она ещё и этот баг выявила.
 
 const userService = new UserService();
 userService.users = 1;
 console.log(userService.users);
-
-const userService2 = new UserService();
-console.log(userService2.users);
-userService2.users = 7
-console.log(userService2.users);
